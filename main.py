a0fcc3857f667524013e2b4039aa012414751033
AutoKill <milanovicn257@gmail.com>
2021-06-08 13:30:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+f = open('predmeti.json',)
+data = json.load(f)
+ocene = []
+for i in data['predmeti']:
+    print(i + ":")
+    ocena = input()
+    ocene.append(int(ocena))
+f.close()
+zbir = 0
+for i in ocene:
+    zbir += i
+
+print("PROSEK:")
+print(zbir / len(ocene))
